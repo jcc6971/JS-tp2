25f813e68ab36aeebf653ca02fa4afd984cd5de2
jcc <jccollart@outlook.be>
2023-03-22 08:35:02 +0100

    ok

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+const navbar=document.querySelector("#navbar");
+
+
+
+let vertical =0;
+document.addEventListener("scroll", () =>{
+          
+    if (window.scrollY < vertical) {navbar.style.top= 0}
+    else {navbar.style.top= "-60px"};
+    
+    vertical =window.scrollY;
+})
+
+    
+    
+
+
+
+
 // Créer un événément au scroll
 
 // Cacher la navbar si l'utilisateur comment à descendre et la sortir quand il remonte
